2410332dca1bea9d0f7c3907090f1270b677feaf
Pawel Pamula <pawelpamula003@gmail.com>
2023-09-15 21:47:08 +0200

    stripe Integration

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -20,6 +20,8 @@ import {
 } from "./ui/dialog";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const tools = [
   {
@@ -56,6 +58,21 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      console.log("co sie odpierdala");
+      const response = await axios.get("/api/stripe");
+      console.log("gówno");
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
@@ -86,7 +103,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="upgrade" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="upgrade"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
